bf80f2cd8a79fbd3f6789b547e081f1bcb36e7ad
belyashnikovatn <101242667+belyashnikovatn@users.noreply.github.com>
2024-07-30 13:53:39 +0500

    add validators

--- api_yamdb/content/models.py
+++ api_yamdb/content/models.py
@@ -1,40 +1,69 @@
 from datetime import datetime
 
+from django.contrib.auth import get_user_model
+from django.core.validators import (
+    MinValueValidator,
+    MaxValueValidator,
+    RegexValidator
+)
 from django.db import models
 
 
-class Genre(models.Model):
-    """Класс модели данных для жанров."""
+User = get_user_model()
+
+
+class Detail(models.Model):
+    """Абстрактный класс для категории и жанра."""
     name = models.CharField('Название', max_length=256)
-    slug = models.SlugField('slug', unique=True, max_length=50)
+    slug = models.SlugField(
+        'slug',
+        unique=True,
+        max_length=50,
+        validators=[
+            RegexValidator(
+                regex=r'^[-a-zA-Z0-9_]+$',
+                message='Unacceptable slug'
+            )
+        ]
+    )
+
+    def __str__(self):
+        return self.name
+
+
+class Genre(Detail):
+    """Класс модели данных для жанров."""
 
     class Meta:
         verbose_name = 'жанр'
         verbose_name_plural = 'Жанры'
         ordering = ('name',)
 
-    def __str__(self):
-        return self.name
 
-
-class Category(models.Model):
+class Category(Detail):
     """Класс модели данных для категорий."""
-    name = models.CharField('Название', max_length=256)
-    slug = models.SlugField('slug', unique=True, max_length=50)
 
     class Meta:
         verbose_name = 'категория'
         verbose_name_plural = 'Категории'
         ordering = ('name',)
 
-    def __str__(self):
-        return self.name
-
 
 class Title(models.Model):
     """Класс модели данных для произведений."""
     name = models.CharField('Название', max_length=256)
-    year = models.PositiveIntegerField('Год выпуска',)
+    year = models.PositiveIntegerField(
+        'Год выпуска',
+        validators=[
+            MinValueValidator(
+                1895,
+                message='This is not possible!'),
+            MaxValueValidator(
+                int(datetime.now().year),
+                message='This is not possible!'
+            )
+        ]
+    )
     description = models.TextField('Описание')
     genre = models.ManyToManyField(
         Genre,
@@ -82,7 +111,13 @@ class GenreTitle(models.Model):
 class Review(models.Model):
     """Класс модели данных для отзывов."""
     text = models.TextField('Текст отзыва')
-    score = models.PositiveIntegerField('Оценка'),
+    score = models.PositiveIntegerField(
+        'Оценка',
+        validators=[
+            MinValueValidator(1, message='More'),
+            MaxValueValidator(10, message='Less')
+        ]
+    )
     pub_date = models.DateTimeField(
         verbose_name='Дата и время публикации, автоматически',
         auto_now_add=True,
@@ -93,6 +128,12 @@ class Review(models.Model):
         on_delete=models.CASCADE,
         related_name='reviews',
         verbose_name='Произведение'
+    )
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='reviews',
+        verbose_name='Автор'
     )
 
     class Meta:
@@ -117,6 +158,12 @@ class Comment(models.Model):
         on_delete=models.CASCADE,
         related_name='comments',
         verbose_name='отзыв'
+    )
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='comments',
+        verbose_name='Автор'
     )
     text = models.TextField('Текст комментария')
     pub_date = models.DateTimeField(
