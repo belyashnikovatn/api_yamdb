40c14e8a84351a9cfe3c73394f08924c94bc45dd
Artöm <artyom-ivanov-spb@yandex.ru>
2024-07-31 22:27:12 +0300

    Build draft routes for auth and user managment. (#2)
    * Build draft routes for auth and user managment.
    * Fixed url in router.

---
+++ api_yamdb/api/urls.py
@@ -0,0 +1,15 @@
+from django.urls import include, path
+from rest_framework import routers
+
+# from .views import SignUpView, TokenView, UsersViewSet
+
+API_VERSION_1 = 'v1/'
+
+router_v1 = routers.DefaultRouter()
+# router_v1.register('users', UsersViewSet)
+
+urlpatterns = [
+    # path(f'{API_VERSION_1}auth/signup/', SignUpView.as_view(), name='signup'),
+    # path(f'{API_VERSION_1}auth/token/', TokenView.as_view(), name='get_token'),
+    # path(f'{API_VERSION_1}', include(router_v1.urls)),
+]
