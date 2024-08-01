096a3946ef450d53be89816231a636dffd6c88c4
Tania Belyashnikova <101242667+belyashnikovatn@users.noreply.github.com>
2024-08-01 23:30:02 +0200

    mixins.DestroyModelMixin!!! а не mixins.Destroy (#4)
    * fix bug: Title.description not required & add reviews_db.json data for testing

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -2,17 +2,17 @@ from rest_framework import viewsets
 from rest_framework import filters, mixins, viewsets
 from reviews.models import Title, Genre, Category
 from api.serializers import (
-    GenreSerailizer,
-    CategorySerailizer,
-    TitleSerailizer,
-    TitleReadOnlySerailizer
+    GenreSerializer,
+    CategorySerializer,
+    TitleSerializer,
+    TitleReadOnlySerializer
 )
 from django_filters.rest_framework import DjangoFilterBackend
 
 
 class NameSlugModelViewSet(mixins.CreateModelMixin,
                            mixins.ListModelMixin,
-                           mixins.Destroy,
+                           mixins.DestroyModelMixin,
                            viewsets.GenericViewSet):
     """Абстрактный класс для вьюсетов категория/жанр."""
     filter_backends = (filters.SearchFilter,)
@@ -20,25 +20,31 @@ class NameSlugModelViewSet(mixins.CreateModelMixin,
 
 
 class GenreViewSet(NameSlugModelViewSet):
-    """Вьюсет для жанра."""
-    serializer_class = GenreSerailizer
+    """Вьюсет для жанра.
+    Доступные действия: просмотр списка, добавление, удаление,
+    поиск по наименованию (регистр учитывается)."""
+    serializer_class = GenreSerializer
     queryset = Genre.objects.all()
 
 
 class CategoryViewSet(NameSlugModelViewSet):
-    """Вьюсет для категории."""
-    serializer_class = CategorySerailizer
+    """Вьюсет для категории.
+    Доступные действия: просмотр списка, добавление, удаление,
+    поиск по наименованию (регистр учитывается)."""
+    serializer_class = CategorySerializer
     queryset = Category.objects.all()
 
 
 class TitleViewSet(viewsets.ModelViewSet):
-    """Вьюсет для произведений."""
-    serializer_class = TitleSerailizer
+    """Вьюсет для произведений.
+    Доступные действия: весь набор.
+    Поиск по полям: название, год, slug жанры(ы), slug категория."""
+    serializer_class = TitleSerializer
     queryset = Title.objects.all()
     filter_backends = (DjangoFilterBackend,)
     filterset_fields = ('name', 'year', 'genre__slug', 'category__slug')
 
     def get_serializer_class(self):
         if self.request.method == 'GET':
-            return TitleReadOnlySerailizer
-        return TitleSerailizer
+            return TitleReadOnlySerializer
+        return TitleSerializer
