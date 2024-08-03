98f0dd97886f8b9a984352623b86cdc8c6e8f13d
Artöm <artyom-ivanov-spb@yandex.ru>
2024-08-03 23:13:52 +0300

    Feature/auth views (#8)
    * Adjust Users model and set up token.
    
    * Build SignUpView, TokenView, UserViewSet + draft serializers.
    
    * Add pagination on the Project Level

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -1,8 +1,49 @@
-from reviews.models import Category, Comment, Genre, Title, Review
+from rest_framework import serializers
+
 from django.core.exceptions import ValidationError
 from django.shortcuts import get_object_or_404
 
-from rest_framework import serializers
+from reviews.models import Category, Comment, Genre, Title, Review
+from users.models import User
+
+
+
+class SignUpSerializer(serializers.ModelSerializer):
+    """
+    Класс-сериализатор для регистрации пользователей.
+    Проверяет уникальность email и username.
+    """
+    class Meta:
+        model = User
+        fields = ['email', 'username']
+    pass
+
+
+class TokenSerializer(serializers.Serializer):
+    """
+    Класс-сериализатор для получения токена JWT.
+    Проверяет корректность username и кода подтверждения.
+
+    Наследуем от сериализатора класса Serializer, а не модели, так как:
+    - не требуется создавать или обновлять объекты модели.
+    - требуется только проводить валидацию переданных значений.
+    """
+    username = serializers.CharField(max_length=150)
+    confirmation_code = serializers.CharField(max_length=100)
+    pass
+
+
+class UserSerializer(serializers.ModelSerializer):
+    """
+    Класс-сериализатор для переопределенной модели пользователя.
+
+    Применяются валидации и ограничения, указанные в модели User.
+    Дополнительные специфические валидации не треубуются.
+    """
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'email', 'first_name',
+                  'last_name', 'bio', 'role']
 
 
 class GenreSerializer(serializers.ModelSerializer):
--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,13 +1,163 @@
 from rest_framework import viewsets
 from rest_framework import filters, mixins, viewsets
+from rest_framework import generics
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework_simplejwt.tokens import AccessToken
+from rest_framework.filters import SearchFilter
+from rest_framework.decorators import action
+from rest_framework.pagination import PageNumberPagination
+
 from reviews.models import Title, Genre, Category
+from users.models import User
 from api.serializers import (
+    SignUpSerializer,
+    TokenSerializer,
+    UserSerializer,
     GenreSerializer,
     CategorySerializer,
     TitleSerializer,
     TitleReadOnlySerializer
 )
 from django_filters.rest_framework import DjangoFilterBackend
+import random
+import string
+from django.core.mail import send_mail
+from django.conf import settings
+
+
+class SignUpView(generics.CreateAPIView):
+    """
+    Вью для регистрации пользователя.
+    Этот класс обрабатывает только POST-запросы которые
+    приходят на эндпоинт /api/v1/auth/signup/.
+
+    Данный вью-класс использует сериализатор SignUpSerializer для
+    валидации значений, переданных пользователем.
+
+    Создает нового пользователя и отправляет код подтверждения.
+
+    Модель, в которой будет создан новый
+    пользователь определяется в сериализаторе.
+    """
+    serializer_class = SignUpSerializer
+
+    def perform_create(self, serializer):
+        """
+        Создаем нового пользователя и сразу отправляем ему
+        код подтверждения на email.
+
+        :param serializer: Сериализатор с данными пользователя.
+        """
+        user = serializer.save()
+        confirmation_code = ''.join(
+            random.choices(string.ascii_letters + string.digits, k=20))
+        user.confirmation_code = confirmation_code
+        user.save()
+        send_mail(
+            'Ваш код подтверждения',
+            f'Ваш код подтверждения {confirmation_code}',
+            settings.DEFAULT_FROM_EMAIL,
+            [user.email],
+            fail_silently=False,
+        )
+
+
+class TokenView(generics.GenericAPIView):
+    """
+    Вью для получения JWT-токена.
+
+    Этот класс обрабатывает только POST-запросы которые
+    приходят на эндпоинт /api/v1/auth/token/.
+
+    Данный вью-класс использует сериализатор TokenSerializer для
+    валидации значений, переданных пользователем.
+
+    Принимает параметры username и confirmation_code от юзера,
+    и возвращает JWT access-токен.
+
+    Для обновления access-токена не применяем refresh-токен и доп. эндпоинт.
+    Токен обновляется через повторную передачу username и кода подтверждения.
+    """
+    serializer_class = TokenSerializer
+
+    def create(self, request, *args, **kwargs):
+        """
+        Создает JWT-токен на основе username и confirmation_code.
+
+        :param request: Входной запрос.
+        :return: Ответ с JWT токеном или ошибкой валидации.
+        """
+        # Создаем экземпляр сериализатора с данными из POST-запроса:
+        serializer = self.get_serializer(data=request.data)
+        # Проверяем, что данные POST-запроса валидны.
+        # Если нет, выбрасывается ValidationError:
+        serializer.is_valid(raise_exception=True)
+        # Извлекаем успешно провалидированные
+        # значения из validated_data в сериализаторе TokenSerializer:
+        username = serializer.validated_data['username']
+        confirmation_code = serializer.validated_data['confirmation_code']
+
+        # Логика проверки и создания токена
+        try:
+            # В момент регестрации (SignUpView) эти поля пропишутся в Users
+            user = User.objects.get(username=username,
+                                    confirmation_code=confirmation_code)
+        except User.DoesNotExist:
+            return Response({'detail': 'Неверное имя или код подтверждения'},
+                            status=status.HTTP_400_BAD_REQUEST)
+
+        # Создание объекта AccessToken для пользователя
+        access_token = AccessToken.for_user(user)
+
+        return Response({'access': str(access_token),
+                         }, status=status.HTTP_200_OK)
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    """
+    Вьюсет для управления CRUD операциями Пользователей.
+
+    GET /api/v1/users/: Получение списка юзеров
+    с возможностью поиска по username.
+    POST /api/v1/users/: Создание нового юзера.
+    GET /api/v1/users/{username}/: Получение данных юзера по username.
+    PATCH /api/v1/users/{username}/: Обновление данных юзера по username.
+    DELETE /api/v1/users/{username}/: Удаление юзера.
+
+    Кастомные методы:
+    GET /api/v1/users/me/: Получение данных текущего юзера.
+    PATCH /api/v1/users/me/: Обновление данных текущего юзера.
+    """
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    # permission_classes -- пока что оставлю заглушку.
+    pagination_class = PageNumberPagination
+    filter_backends = (SearchFilter,)
+    search_fields = ('username',)
+
+    @action(detail=False, methods=['get', 'patch'], url_path='me')
+    def me(self, request):
+        """
+        Обрабатывает GET и PATCH запросы для
+        текущего аутентифицированного пользователя.
+        """
+        if request.method == 'GET':
+            # Создаем экземпляр сериализатора с данными из POST-запроса:
+            serializer = self.get_serializer(request.user)
+            return Response(serializer.data)
+
+        elif request.method == 'PATCH':
+            # Создаем экземпляр сериализатора с данными из POST-запроса.
+            # partial=True -- поскольку у нас PATCH-запрос, ставим флаг
+            # что мы обновляем не все поля, как в PUT-запросе, а выборочные.
+            serializer = self.get_serializer(request.user,
+                                             data=request.data,
+                                             partial=True)
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data)
+            return Response(serializer.errors, status=400)
 
 
 class NameSlugModelViewSet(mixins.CreateModelMixin,
