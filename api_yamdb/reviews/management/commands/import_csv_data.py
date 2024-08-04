b1b7fe56eb464750acbdca8bcf93c62f22033e47
belyashnikovatn <101242667+belyashnikovatn@users.noreply.github.com>
2024-08-04 14:17:40 +0500

    Yeah! Refactoring done!

--- api_yamdb/reviews/management/commands/import_csv_data.py
+++ api_yamdb/reviews/management/commands/import_csv_data.py
@@ -1,12 +1,59 @@
 import csv
 import os
+import logging
 
 from django.conf import settings
 from django.core.management.base import BaseCommand
 
 from api_yamdb.settings import CSV_FILES_DIRS
-from reviews.models import Genre, Category, Title, GenreTitle, NameSlugModel
+from reviews.models import (
+    Category,
+    Genre,
+    GenreTitle,
+    NameSlugModel,
+    Title,
+    Review
+)
 from users.models import User
+
+
+logging.basicConfig(
+    format='%(asctime)s - %(funcName)s - %(levelname)s - %(message)s',
+    level=logging.INFO,
+)
+
+# словарь для ссылок на объекты моделей
+FKEYS = {
+    'category': ('category', Category),
+    'author': ('author', User),
+}
+
+
+def clean(model_name):
+    """Удаляет данные из таблиц, чтобы не было ошибок на дубли итд."""
+    try:
+        model_name.objects.all().delete()
+        logging.info(f'Данные из таблицы {model_name} удалены.')
+    except Exception as error:
+        logging.error(f'Ошибка {error}!')
+
+
+def load(csv_file, model):
+    """Загружает данные из файла через модель."""
+    with open(os.path.join(CSV_FILES_DIRS, csv_file),
+              encoding='utf-8') as csvfile:
+        reader = csv.DictReader(csvfile)
+        try:
+            for row in reader:
+                # проверка на ссылку на объект
+                if (k := set(row.keys()) & set(FKEYS)):
+                    for i in list(k):
+                        row[i] = FKEYS[i][1].objects.get(pk=row[i])
+                entity = model(**row)
+                entity.save()
+            logging.info(f'Данные "{csv_file}" загружены.')
+        except Exception as error:
+            logging.error(f'Ошибка {error}! Смотри {row.keys()}')
 
 
 class Command(BaseCommand):
@@ -15,23 +62,17 @@ class Command(BaseCommand):
     help = 'Import csv-files into DB through models.'
 
     def handle(self, *args, **options):
-        # Clean tables before.
-        NameSlugModel.objects.all().delete()
-        Title.objects.all().delete()
+        # Clean the tables before.
+        tables_to_clean = [NameSlugModel, GenreTitle, Review, Title, User]
+        [clean(table) for table in tables_to_clean]
+
+        # Import data into cleaned tables through ORM.
         files_models = {
             'category.csv': Category,
             'genre.csv': Genre,
-            # 'genre_title.csv': GenreTitle,
-            # 'titles.csv': Title
+            'titles.csv': Title,
+            'genre_title.csv': GenreTitle,
+            'users.csv': User,
+            'review.csv': Review,
         }
-
-        for csv_file, model in files_models.items():
-            with open(os.path.join(CSV_FILES_DIRS, csv_file),
-                      encoding='utf-8') as csvfile:
-                reader = csv.DictReader(csvfile)
-                for row in reader:
-                    model.objects.create(**row)
-
-            self.stdout.write(self.style.SUCCESS(
-                f'{model.name} data imported successfully from {csv_file}'
-            ))
+        [load(csv_file, model) for csv_file, model in files_models.items()]
